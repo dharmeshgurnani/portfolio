59d414266a0233bbdb1d36145e7b755c05dc5ee3
dharmeshgurnani <dharmeshgurnani2000@gmail.com>
2022-07-24 15:52:23 +0530

    feat-revised animations and added new ones

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import $ from 'jquery';
 //import HeroImg from '../assets/images/hero.png';
 import Button from './Button';
 import ScrollDownArrow from '../assets/images/scroll-down-arrow.svg';
 import PText from './PText';
 import '../style/components/HeroSection.scss'
+import { gsap, Power4 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function HeroSection() {
+
+  useEffect(() => {
+    gsap.fromTo(
+      '.hero__name__superScript', {
+      x: window.innerWidth
+    }, {
+      x: 0,
+      ease: Power4.easeOut,
+      duration: 1.5
+    }
+    );
+
+    gsap.fromTo(
+      '.hero__name', {
+      x: window.innerWidth
+    }, {
+      x: 0,
+      ease: Power4.easeOut,
+      duration: 1.7
+    }
+    );
+
+    gsap.fromTo(
+      '.hero__oneliner', {
+      x: -(window.innerWidth)
+    }, {
+      x: 0,
+      ease: Power4.easeOut,
+      duration: 1.5
+    }
+    );
+
+    gsap.fromTo(
+      '.hero__info .para', {
+      x: (window.innerWidth)
+    }, {
+      x: 0,
+      ease: Power4.easeOut,
+      duration: 1.7
+    }
+    );
+
+    gsap.fromTo(
+      '.hero__info .button-wrapper', {
+      x: -(window.innerWidth)
+    }, {
+      x: 0,
+      ease: Power4.easeOut,
+      duration: 1.5
+    }
+    );
+  }, []);
+  //word animation
   setTimeout(function () {
     var sectionHome = $("[class^='rubberWords']");
     sectionHome.find(".word").removeClass('animated bounceIn');
@@ -25,7 +81,7 @@ export default function HeroSection() {
       <div className="hero">
         <div className="container">
           <h1 className="hero__heading">
-            <span>Hello, I'm</span>
+            <span className='hero__name__superScript'>Hello, I'm</span>
             <span className="rubberWords hero__name">
               <span className="sm-screen-only animation_rubber">Dharmesh Gurnani</span>
               <span className="word animation_rubber">D</span>
@@ -56,7 +112,7 @@ export default function HeroSection() {
                 I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently,I'm focused on building accessible, human-centered products on Salesforce Commerce Cloud,I'm passionate about programming and Networking.
               </PText>
             </div>
-            <Button btnText="see my works" btnLink="/projects" />
+            <Button className="hero__button" btnText="see my works" btnLink="/projects" />
           </div>
           <div className="hero__scrollDown right animation_bounce-1">
             <p>Scroll</p>
