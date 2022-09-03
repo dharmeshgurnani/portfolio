3d45ee9cb67bdc0fa554cae8537a90e42d3a7ed8
dharmeshgurnani <dharmeshgurnani2000@gmail.com>
2022-09-03 21:50:19 +0530

    feat-add PWA details

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import CustomCursor from './components/CustomCursor';
 import CustomCursorManager from './components/CustomCursor/context/manager.tsx';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './style/GlobalStyle.scss';
 import './style/Typography.scss';
@@ -17,6 +18,12 @@ ReactDOM.render(
   </CustomCursorManager>,
   document.getElementById('root')
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
