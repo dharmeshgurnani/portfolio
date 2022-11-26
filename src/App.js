d3058dd27a95abe04d9af93f88ef94d8a4720b98
dharmeshgurnani <dharmeshgurnani2000@gmail.com>
2022-11-26 19:14:53 +0530

    fix(updated routes to v6)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavMenu from './components/NavMenu';
 import Home from './pages/home';
-import {BrowserRouter, Routes, Route,} from "react-router-dom";
+import {BrowserRouter, Routes, Route,Link} from "react-router-dom";
 import Projects from './pages/projects';
 //import Game from './pages/game';
 import Contact from './pages/contact';
@@ -9,13 +9,14 @@ import Footer from './components/Footer';
 function App() {
   return (
     <BrowserRouter>
+    <Link path="/portfolio/:contact/"/>
       <NavMenu />
       <div className="mainContent">
         <Routes>
-          <Route path="/portfolio" element={<Home />} />
-          <Route path="/portfolio/:projects/*" element={<Projects />} />
-          <Route path="/portfolio/:contact/*" element={<Contact />} />
-          <Route path="/portfolio/*" element={<Home />} />
+          <Route path=":portfolio/" element={<Home />} />
+          <Route path=":portfolio/projects" element={<Projects />} />
+          <Route path=":portfolio/contact" element={<Contact />} />
+          <Route path=":portfolio/*" element={<Home />} />
         </Routes>
       </div>
       <Footer />
